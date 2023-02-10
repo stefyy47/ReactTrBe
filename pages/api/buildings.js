a4a6d86a7d3f7cfdaa93e1a3c7e60f53be21c8a8
stefyy47 <stefanpogonaru7@gmail.com>
2023-02-11 01:15:17 +0200

    abc

--- pages/api/buildings.js
+++ pages/api/buildings.js
@@ -4,7 +4,7 @@ export default async function handler(request, response) {
   const { database } = await connectToDatabase();
   const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
 
-  const results = await collection.find({});
+  const results = await collection.findOne({});
 
   response.status(200).json(results);
 }
