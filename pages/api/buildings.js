cafe701ef3bf40074b91bb7c4cc6c2ad21313e52
stefyy47 <stefanpogonaru7@gmail.com>
2023-02-11 04:41:35 +0200

    change

--- pages/api/buildings.js
+++ pages/api/buildings.js
@@ -3,6 +3,6 @@ import { connectToDatabase } from "../../lib/mongodb";
 export default async function handler(request, response) {
   const { database } = await connectToDatabase();
   const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
-  const result = await collection.findOne(request.query);
+  const result = await collection.find(request.query).toArray();
   response.status(200).json(result);
 }
